04b91b4c25968568264d34db53cc56d2212fcf2e
Anna <ann.kazannik@gmail.com>
2023-05-31 23:34:03 -0700

    Table for listings on the buyer

--- src/components/Listings/index.tsx
+++ src/components/Listings/index.tsx
@@ -1,13 +1,82 @@
-import { Box, Container } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  Table,
+  TableContainer,
+  Td,
+  Th,
+  Tbody,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { FC } from "react";
+import { useAccount, useContractRead } from "wagmi";
 
+import { abi, contractAddress } from "../../../constants/croplink";
+import LoadingPage from "../LoadingPage";
 import Sidebar from "../Sidebar";
 
-export default function Listings() {
+type Product = {
+  name: string;
+  price: BigInt;
+  quantity: BigInt;
+};
+
+const Listings: FC = () => {
+  const {
+    data,
+    isLoading: isListLoading,
+    refetch,
+  } = useContractRead({
+    address: contractAddress,
+    abi,
+    functionName: "produceList",
+  }) as any;
+
+  if (isListLoading) return <LoadingPage />;
+
   return (
     <Box>
       <Container maxW={"7xl"} mt="10">
-        <Sidebar>Listings</Sidebar>
+        <Sidebar>
+          <Box>
+            <Flex
+              justifyContent="space-between"
+              alignItems="center"
+              marginBottom="5"
+            >
+              <Heading size="lg">Listings</Heading>
+            </Flex>
+          </Box>
+
+          <TableContainer>
+            <Table variant="simple">
+              <Thead>
+                <Tr>
+                  <Th>Product</Th>
+                  <Th>Price</Th>
+                  <Th>Stock</Th>
+                  <Th></Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {data?.map((product: Product, i: number) => (
+                  <Tr key={i}>
+                    <Th>{product.name}</Th>
+                    <Th>{product.price.toString()}</Th>
+                    <Th>{product.quantity.toString()}</Th>
+                    <Th></Th>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Sidebar>
       </Container>
     </Box>
   );
-}
+};
+
+export default Listings;
